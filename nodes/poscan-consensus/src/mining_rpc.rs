1b8b6ad0bf734565b3d3e7ce345e454a1f554531
3dpass <3dpass@tuta.io>
2022-05-07 16:43:49 +0300

    Limit mining object quue len

--- nodes/poscan-consensus/src/mining_rpc.rs
+++ nodes/poscan-consensus/src/mining_rpc.rs
@@ -7,6 +7,11 @@ extern crate alloc;
 use alloc::string::String;
 use crate::service::{MiningProposal, DEQUE};
 
+const MAX_QUEUE_LEN: usize = 5;
+
+const RES_OK: u64 = 0;
+const RES_QUEUE_FULL: u64 = 1;
+
 #[rpc]
 pub trait PoscanMiningRpc {
 	#[rpc(name = "push_mining_object")]
@@ -20,8 +25,11 @@ impl PoscanMiningRpc for MiningRpc {
 	fn push(&self, obj_id: u64, obj: String) -> Result<u64> {
 		info!(">>> push_minig_obj recieved object obj_id={}", obj_id);
 		let mut lock = DEQUE.lock();
+		if lock.len() >= MAX_QUEUE_LEN {
+			return Ok(RES_QUEUE_FULL)
+		}
 		(*lock).push_back(MiningProposal {id: 1, pre_obj: obj.as_bytes().to_vec()});
 
-		Ok(0)
+		Ok(RES_OK)
 	}
 }
