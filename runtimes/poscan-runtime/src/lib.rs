a474944dbc2d0a43ace1587064ec2b86846d6bf1
3dpass <3dpass@tuta.io>
2022-05-14 20:58:21 +0300

    Remove unnecessary nodes and runtimes

--- runtimes/poscan-runtime/src/lib.rs
+++ runtimes/poscan-runtime/src/lib.rs
@@ -116,8 +116,8 @@ pub mod opaque {
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("minimal-grandpa-runtime"),
-	impl_name: create_runtime_str!("minimal-grandpa-runtime"),
+	spec_name: create_runtime_str!("poscan-runtime"),
+	impl_name: create_runtime_str!("poscan-runtime"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
