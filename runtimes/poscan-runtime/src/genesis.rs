9d7a28384b1cc9fbee0228b48ad2e276360867ae
3dpass <3dpass@tuta.io>
2022-05-27 15:22:17 +0300

    Change rewards from 60 to 50

--- runtimes/poscan-runtime/src/genesis.rs
+++ runtimes/poscan-runtime/src/genesis.rs
@@ -77,7 +77,7 @@ pub fn testnet_genesis(
 			initial_difficulty,
 		}),
 		rewards: Some(RewardsConfig {
-			reward: 60 * DOLLARS,
+			reward: 50 * DOLLARS,
 			mints: Default::default(),
 		}),
 		collective_Instance1: Default::default(),
