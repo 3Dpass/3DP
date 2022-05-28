fd5ea59c47b357a28296ddc59320f9e71974dd9b
3DPass - The Ledger of unique things <73595107+3Dpass@users.noreply.github.com>
2022-05-28 09:33:33 -0700

    Update mod.rs

--- runtimes/poscan-runtime/src/weights/mod.rs
+++ runtimes/poscan-runtime/src/weights/mod.rs
@@ -16,7 +16,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Kulupu. If not, see <http://www.gnu.org/licenses/>.
+// along with 3DPass. If not, see <http://www.gnu.org/licenses/>.
 
 //! Weights for pallets used in this runtime.
 
