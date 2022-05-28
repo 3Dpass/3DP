6be49735e800ab29d217c66ed7ab821e69f2b3d8
3DPass - The Ledger of unique things <73595107+3Dpass@users.noreply.github.com>
2022-05-28 09:30:04 -0700

    Copyright

--- runtimes/poscan-runtime/src/weights/mod.rs
+++ runtimes/poscan-runtime/src/weights/mod.rs
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// This file is part of Kulupu.
+// This file is part of 3DPass.
 //
 // Copyright (c) 2020 Wei Tang.
 // Copyright (c) 2020 Shawn Tabrizi.
+// Copyright (c) 2022 3DPass.
 //
-// Kulupu is free software: you can redistribute it and/or modify
+// 3DPass is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Kulupu is distributed in the hope that it will be useful,
+// 3DPass is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
