9102ed0558cbc09f8f044c2e4708ee8623c7ed4b
3dpass <3dpass@tuta.io>
2022-05-31 18:32:30 +0300

    Adjust total supply

--- primitives/consensus/poscan/src/lib.rs
+++ primitives/consensus/poscan/src/lib.rs
@@ -60,7 +60,7 @@ pub const MIN_DIFFICULTY: u128 = DIFFICULTY_DAMP_FACTOR;
 pub const MAX_DIFFICULTY: u128 = u128::max_value();
 
 /// Value of 1 KLP.
-pub const DOLLARS: u128 = 1_000_000_000_000;
+pub const DOLLARS: u128 = 1_000_000_000;
 /// Value of cents relative to KLP.
 pub const CENTS: u128 = DOLLARS / 100;
 /// Value of millicents relative to KLP.
