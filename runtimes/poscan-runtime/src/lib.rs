213fba7e25787fa786db89f116d8712406901fb3
3dpass <3dpass@tuta.io>
2022-06-02 13:24:33 +0300

    Set ExistentialDeposit=0

--- runtimes/poscan-runtime/src/lib.rs
+++ runtimes/poscan-runtime/src/lib.rs
@@ -221,7 +221,7 @@ impl pallet_timestamp::Config for Runtime {
 }
 
 parameter_types! {
-	pub const ExistentialDeposit: u128 = 500;
+	pub const ExistentialDeposit: u128 = 0;
 	pub const TransferFee: u128 = 0;
 	pub const CreationFee: u128 = 0;
 	pub const MaxLocks: u32 = 50;
