2f90630c4174b1505cf73d2c6c8cc69741efdb10
MikhailK <mk@hotmail.ru>
2022-07-27 17:19:11 +0300

    Fix difficulty

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -277,7 +277,7 @@ parameter_types! {
 impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
-	type OnTimestampSet = ();
+	type OnTimestampSet = Difficulty;
 	type MinimumPeriod = MinimumPeriod;
 	type WeightInfo = ();
 }
