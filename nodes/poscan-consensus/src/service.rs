81e11e441b9bf4a372e764a2b933a03f6bfe9ce4
MikhailK <mk@hotmail.ru>
2023-01-25 23:55:16 +0300

    Mining v2

--- nodes/poscan-consensus/src/service.rs
+++ nodes/poscan-consensus/src/service.rs
@@ -22,7 +22,7 @@ use sp_keystore::{SyncCryptoStore, SyncCryptoStorePtr};
 use sp_runtime::traits::Block as BlockT;
 use sp_core::crypto::{Ss58Codec,UncheckedFrom, Ss58AddressFormat, set_default_ss58_version};
 use sp_core::Pair;
-use sp_consensus_poscan::{POSCAN_COIN_ID,POSCAN_ALGO_GRID2D} ;
+use sp_consensus_poscan::{POSCAN_COIN_ID, POSCAN_ALGO_GRID2D_V2} ;
 use async_trait::async_trait;
 use sc_rpc::SubscriptionTaskExecutor;
 use sc_finality_grandpa::{FinalityProofProvider as GrandpaFinalityProofProvider};
@@ -427,13 +427,13 @@ pub fn new_full(
 						let maybe_mining_prop = (*lock).pop_front();
 						drop(lock);
 						if let Some(mp) = maybe_mining_prop {
-							let hashes = get_obj_hashes(&POSCAN_ALGO_GRID2D, &mp.pre_obj, &metadata.best_hash);
+							let hashes = get_obj_hashes(&POSCAN_ALGO_GRID2D_V2, &mp.pre_obj, &metadata.best_hash);
 							if hashes.len() > 0 {
 								let obj_hash = hashes[0];
 								let dh = DoubleHash { pre_hash: metadata.pre_hash, obj_hash };
 								poscan_hash = dh.calc_hash();
 								poscan_data = Some(PoscanData {
-									alg_id: POSCAN_ALGO_GRID2D,
+									alg_id: POSCAN_ALGO_GRID2D_V2,
 									hashes, obj:
 									mp.pre_obj
 								});
