eb9798e02332cd51f825a3693bdb4b95e216dc8e
Paul <107915078+PaulSff@users.noreply.github.com>
2024-07-18 10:56:06 -0700

    add keygen scripts
    These scripts will help to generate all the keys required for the Node to operate.

---
+++ keygen_seed.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# A keyset will be generated out of the seed phrase below
+MEMO_SEED="PUT YOUR MEMO SEED HERE"
+
+# inspecting the account
+ACCOUNT="$(./target/release/poscan-consensus key inspect --scheme Sr25519 "$MEMO_SEED")"
+echo "----Mining account----"
+echo "$ACCOUNT"
+# importing mining key
+./target/release/poscan-consensus import-mining-key "$MEMO_SEED" --base-path ~/3dp-chain/ --chain mainnetSpecRaw.json
+# deriving GRANDPA key from the seed phrase
+GRANDPA_KEY="$(./target/release/poscan-consensus key inspect --scheme Ed25519 "$MEMO_SEED" | sed -n 's/.*Secret seed://p')"
+GRANDPA_ACCOUNT="$(./target/release/poscan-consensus key inspect --scheme Ed25519 "$MEMO_SEED")"
+echo "---- GRANDPA key derived ----:"
+echo "$GRANDPA_ACCOUNT"
+# inserting GRANDPA key
+./target/release/poscan-consensus key insert --scheme Ed25519 --base-path ~/3dp-chain/ --chain mainnetSpecRaw.json --key-type gran --suri $GRANDPA_KEY
+# inserting ImOnline key
+./target/release/poscan-consensus key insert --scheme Sr25519 --base-path ~/3dp-chain/ --chain mainnetSpecRaw.json --key-type imon --suri $GRANDPA_KEY
+echo "These are the keys in your keystore ~/3dp-chain/chains/3dpass/keystore/..."
+# checking up the keys in the keystore
+ls "../3dp-chain/chains/3dpass/keystore/"
