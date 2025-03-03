3bb5a87bf6fe29357c4669714170373fbfc78f51
MikhailK <mkornotmk@gmail.com>
2025-03-03 17:57:21 +0300

    Add balance precompile

--- runtime/src/precompiles.rs
+++ runtime/src/precompiles.rs
@@ -1,56 +1,146 @@
-use pallet_evm::{Precompile, PrecompileHandle, PrecompileResult, PrecompileSet};
-use sp_core::H160;
-use sp_std::marker::PhantomData;
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
 
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+//use crate::{
+//asset_config::{ForeignAssetInstance, LocalAssetInstance},
+// 	xcm_config::XcmExecutorConfig,
+// 	CouncilInstance, TechCommitteeInstance, TreasuryCouncilInstance,
+//};
+use frame_support::parameter_types;
+//use moonbeam_relay_encoder::westend::WestendEncoder;
+// use pallet_evm_precompile_author_mapping::AuthorMappingPrecompile;
+use pallet_evm_precompile_balances_erc20::{Erc20BalancesPrecompile, Erc20Metadata};
+//use pallet_evm_precompile_batch::BatchPrecompile;
+use pallet_evm_precompile_blake2::Blake2F;
+use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
+//use pallet_evm_precompile_call_permit::CallPermitPrecompile;
+//use pallet_evm_precompile_collective::CollectivePrecompile;
+//use pallet_evm_precompile_crowdloan_rewards::CrowdloanRewardsPrecompile;
+//use pallet_evm_precompile_democracy::DemocracyPrecompile;
 use pallet_evm_precompile_modexp::Modexp;
+//use pallet_evm_precompile_parachain_staking::ParachainStakingPrecompile;
+//use pallet_evm_precompile_proxy::ProxyPrecompile;
+//use pallet_evm_precompile_randomness::RandomnessPrecompile;
+//use pallet_evm_precompile_relay_encoder::RelayEncoderPrecompile;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+//use pallet_evm_precompile_xcm_transactor::{
+//	v1::XcmTransactorPrecompileV1, v2::XcmTransactorPrecompileV2,
+//};
+//use pallet_evm_precompile_xcm_utils::XcmUtilsPrecompile;
+//use pallet_evm_precompile_xtokens::XtokensPrecompile;
+use pallet_evm_precompileset_assets_erc20::{Erc20AssetsPrecompileSet, IsForeign, IsLocal};
+use precompile_utils::precompile_set::*;
 
-pub struct FrontierPrecompiles<R>(PhantomData<R>);
+/// ERC20 metadata for the native token.
+pub struct NativeErc20Metadata;
 
-impl<R> FrontierPrecompiles<R>
-where
-	R: pallet_evm::Config,
-{
-	pub fn new() -> Self {
-		Self(Default::default())
-	}
-	pub fn used_addresses() -> [H160; 7] {
-		[
-			hash(1),
-			hash(2),
-			hash(3),
-			hash(4),
-			hash(5),
-			hash(1024),
-			hash(1025),
-		]
-	}
-}
-impl<R> PrecompileSet for FrontierPrecompiles<R>
-where
-	R: pallet_evm::Config,
-{
-	fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult> {
-		match handle.code_address() {
-			// Ethereum precompiles :
-			a if a == hash(1) => Some(ECRecover::execute(handle)),
-			a if a == hash(2) => Some(Sha256::execute(handle)),
-			a if a == hash(3) => Some(Ripemd160::execute(handle)),
-			a if a == hash(4) => Some(Identity::execute(handle)),
-			a if a == hash(5) => Some(Modexp::execute(handle)),
-			// Non-Frontier specific nor Ethereum precompiles :
-			a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
-			a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
-			_ => None,
-		}
+impl Erc20Metadata for NativeErc20Metadata {
+	/// Returns the name of the token.
+	fn name() -> &'static str {
+		"P3Dt token"
 	}
 
-	fn is_precompile(&self, address: H160) -> bool {
-		Self::used_addresses().contains(&address)
+	/// Returns the symbol of the token.
+	fn symbol() -> &'static str {
+		"P3Dt"
+	}
+
+	/// Returns the decimals places of the token.
+	fn decimals() -> u8 {
+		12
+	}
+
+	/// Must return `true` only if it represents the main native currency of
+	/// the network. It must be the currency used in `pallet_evm`.
+	fn is_native_currency() -> bool {
+		true
 	}
 }
 
-fn hash(a: u64) -> H160 {
-	H160::from_low_u64_be(a)
+/// The asset precompile address prefix. Addresses that match against this prefix will be routed
+/// to Erc20AssetsPrecompileSet being marked as foreign
+pub const FOREIGN_ASSET_PRECOMPILE_ADDRESS_PREFIX: &[u8] = &[255u8; 4];
+/// The asset precompile address prefix. Addresses that match against this prefix will be routed
+/// to Erc20AssetsPrecompileSet being marked as local
+pub const LOCAL_ASSET_PRECOMPILE_ADDRESS_PREFIX: &[u8] = &[255u8, 255u8, 255u8, 254u8];
+
+parameter_types! {
+	pub ForeignAssetPrefix: &'static [u8] = FOREIGN_ASSET_PRECOMPILE_ADDRESS_PREFIX;
+	pub LocalAssetPrefix: &'static [u8] = LOCAL_ASSET_PRECOMPILE_ADDRESS_PREFIX;
 }
+
+/// The PrecompileSet installed in the Moonbase runtime.
+/// We include the nine Istanbul precompiles
+/// (https://github.com/ethereum/go-ethereum/blob/3c46f557/core/vm/contracts.go#L69)
+/// as well as a special precompile for dispatching Substrate extrinsics
+/// The following distribution has been decided for the precompiles
+/// 0-1023: Ethereum Mainnet Precompiles
+/// 1024-2047 Precompiles that are not in Ethereum Mainnet but are neither Moonbeam specific
+/// 2048-4095 Moonbeam specific precompiles
+pub type FrontierPrecompiles<R> = PrecompileSetBuilder<
+	R,
+	(
+		// Skip precompiles if out of range.
+		PrecompilesInRangeInclusive<
+			(AddressU64<1>, AddressU64<4095>),
+			(
+				// Ethereum precompiles:
+				// We allow DELEGATECALL to stay compliant with Ethereum behavior.
+				PrecompileAt<AddressU64<1>, ECRecover, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<2>, Sha256, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<3>, Ripemd160, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<4>, Identity, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<5>, Modexp, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<6>, Bn128Add, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<7>, Bn128Mul, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<8>, Bn128Pairing, ForbidRecursion, AllowDelegateCall>,
+				PrecompileAt<AddressU64<9>, Blake2F, ForbidRecursion, AllowDelegateCall>,
+				// Non-Moonbeam specific nor Ethereum precompiles :
+				PrecompileAt<AddressU64<1024>, Sha3FIPS256>,
+				// PrecompileAt<AddressU64<1025>, Dispatch<R>>,
+				PrecompileAt<AddressU64<1026>, ECRecoverPublicKey>,
+				// Moonbeam specific precompiles:
+				// PrecompileAt<AddressU64<2048>, ParachainStakingPrecompile<R>>,
+				// PrecompileAt<AddressU64<2049>, CrowdloanRewardsPrecompile<R>>,
+				PrecompileAt<AddressU64<2050>, Erc20BalancesPrecompile<R, NativeErc20Metadata>>,
+				// PrecompileAt<AddressU64<2051>, DemocracyPrecompile<R>>,
+				// PrecompileAt<AddressU64<2052>, XtokensPrecompile<R>>,
+				// PrecompileAt<AddressU64<2053>, RelayEncoderPrecompile<R, WestendEncoder>>,
+				// PrecompileAt<AddressU64<2054>, XcmTransactorPrecompileV1<R>>,
+				// PrecompileAt<AddressU64<2055>, AuthorMappingPrecompile<R>>,
+				// PrecompileAt<AddressU64<2056>, BatchPrecompile<R>, LimitRecursionTo<2>>,
+				// PrecompileAt<AddressU64<2057>, RandomnessPrecompile<R>>,
+				// PrecompileAt<AddressU64<2058>, CallPermitPrecompile<R>>,
+				// PrecompileAt<AddressU64<2059>, ProxyPrecompile<R>>,
+				// PrecompileAt<AddressU64<2060>, XcmUtilsPrecompile<R, XcmExecutorConfig>>,
+				// PrecompileAt<AddressU64<2061>, XcmTransactorPrecompileV2<R>>,
+				// PrecompileAt<AddressU64<2062>, CollectivePrecompile<R, CouncilInstance>>,
+				// PrecompileAt<AddressU64<2063>, CollectivePrecompile<R, TechCommitteeInstance>>,
+				// PrecompileAt<AddressU64<2064>, CollectivePrecompile<R, TreasuryCouncilInstance>>,
+			),
+		>,
+		// // Prefixed precompile sets (XC20)
+		// PrecompileSetStartingWith<
+		// 	ForeignAssetPrefix,
+		// 	Erc20AssetsPrecompileSet<R, IsForeign, ForeignAssetInstance>,
+		// >,
+		// PrecompileSetStartingWith<
+		// 	LocalAssetPrefix,
+		// 	Erc20AssetsPrecompileSet<R, IsLocal, LocalAssetInstance>,
+		// >,
+	),
+>;
