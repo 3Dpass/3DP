3e594e2837637f1e9b15d298da6cb13661bece84
Paul <107915078+PaulSff@users.noreply.github.com>
2025-05-02 05:18:02 -0700

    Update utils.rs
    Copyright

--- frontier/cli/src/frontier_db_cmd/utils.rs
+++ frontier/cli/src/frontier_db_cmd/utils.rs
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-// This file is part of Frontier.
-//
 // Copyright (c) 2021-2022 Parity Technologies (UK) Ltd.
-//
+// Copyright (c) 2025 3Dpass
+//// This file is part of 3Dpass.
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
